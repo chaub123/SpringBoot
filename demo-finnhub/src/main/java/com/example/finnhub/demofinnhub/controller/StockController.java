d318033d225ea6023b31abaea3b9867e6e008415
chaub123 <kinchau@hotmail.com.hk>
2023-09-20 07:35:23 +0800

    Update

--- demo-finnhub/src/main/java/com/example/finnhub/demofinnhub/controller/StockController.java
+++ demo-finnhub/src/main/java/com/example/finnhub/demofinnhub/controller/StockController.java
@@ -1,6 +1,7 @@
 package com.example.finnhub.demofinnhub.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +18,10 @@ public class StockController {
     @Autowired
     private StockService stockService;
 
+    @GetMapping("/index")
+    public String index(Model model){
+        return "index";
+    }
 
     @GetMapping("/stock")
     public Stock stock(@RequestParam String symbol){
