b5e438db3029e80268b08014cbc84815d0770ebf
chaub123 <kinchau@hotmail.com.hk>
2023-10-04 19:22:05 +0800

    Update

--- finnhub/src/main/java/com/bootcamp/finnhub/exception/StockNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.bootcamp.finnhub.exception;
-
-public class StockNotFoundException extends RuntimeException {
-    public StockNotFoundException(String message){
-        super(message);
-    }
-}
