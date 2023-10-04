918d14248edcabf9ab6e1c04e50c0b64f2572732
chaub123 <kinchau@hotmail.com.hk>
2023-10-04 17:30:12 +0800

    Update

--- personalprofile/src/main/java/com/chaub123/personalprofile/controller/IndexOperation.java
+++ personalprofile/src/main/java/com/chaub123/personalprofile/controller/IndexOperation.java
@@ -3,6 +3,7 @@ package com.chaub123.personalprofile.controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 public interface IndexOperation {
     @GetMapping("/")
     String index(Model model);
