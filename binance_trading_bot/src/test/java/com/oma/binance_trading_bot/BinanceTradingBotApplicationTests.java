af4d214e962cbe24fd1da5ef7b7da0c40dedadd6
chaub123 <kinchau@hotmail.com.hk>
2023-12-20 22:21:40 +0800

    Update

---
+++ binance_trading_bot/src/test/java/com/oma/binance_trading_bot/BinanceTradingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oma.binance_trading_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinanceTradingBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ sandbox/src/Main.java
@@ -0,0 +1,9 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        String str = "iamaboy";
+        System.out.println(str.indexOf('o'));
+        str.substring()
+    }
+}
